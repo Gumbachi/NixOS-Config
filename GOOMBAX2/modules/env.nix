1262f8ba20361956678b57bb7b431de24db0e643
Gumbachi <jaredremsberg@gmail.com>
2024-07-28 23:31:19 -0400

    hyprlock for laptop

--- GOOMBAX2/modules/env.nix
+++ GOOMBAX2/modules/env.nix
@@ -5,7 +5,7 @@
   environment.sessionVariables = {
     CONFIG = "/home/jared/NixOS-Config";
     NIXOS_OZONE_WL = "1";
-    # WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
     MANGOHUD_CONFIG = "position=top-right,frame_timing=0";
   };
   
