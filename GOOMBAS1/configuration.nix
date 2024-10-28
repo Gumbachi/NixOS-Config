9e56839300af7effc2117f4429a6f2399692b43b
Gumbachi <jaredremsberg@gmail.com>
2024-10-28 01:16:09 -0400

    enable basic programs

--- GOOMBAS1/configuration.nix
+++ GOOMBAS1/configuration.nix
@@ -1,24 +1,43 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
+let 
+  modulePath = ./modules/nixos;
+  sharedModulePath = ../shared/modules;
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-programs.git.enable = true;
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+
+    # GOOMBAX1 -- Mandatory
+    # (modulePath + /boot.nix)
+    # (modulePath + /hardware.nix)
+    # (modulePath + /hyprland.nix)
+    # (modulePath + /sound.nix)
+    # (modulePath + /networking.nix)
+
+
+    # GOOMBAS1 -- Optional
+    (modulePath + /programs.nix)
+    (modulePath + /services.nix)
+    (modulePath + /env.nix)
+    # (modulePath + /catppuccin.nix)
+
+
+    # Shared - The same across systems 
+    # (sharedModulePath + /nixvim.nix)
+    (sharedModulePath + /shells.nix)
+    (sharedModulePath + /fonts.nix)
+    # (sharedModulePath + /docker.nix)
+  ];
+
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  networking.hostName = "GOOMBAS1"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "GOOMBAS1";
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
    time.timeZone = "America/New_York";
@@ -56,19 +75,8 @@ programs.git.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jared = {
      isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-        tree
-        helix
-     ];
+     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
    };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
- environment.systemPackages = with pkgs; [
-helix  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
- ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -94,22 +102,6 @@ helix  #   vim # Do not forget to add an editor to edit configuration.nix! The N
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
 
--- GOOMBAS1/modules/nixos/programs.nix
+++ GOOMBAS1/modules/nixos/programs.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }: {
 
+  programs = {
+    git.enable = true;
+    tmux.enable = true;
+    starship.enable = true;
+    yazi.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     helix
-    starship
-    yazi
     btop
   ];
 
