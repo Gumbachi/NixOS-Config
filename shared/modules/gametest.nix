16256e1aae335d221360d4ee9d6ae3f571e63505
Gumbachi <jaredremsberg@gmail.com>
2025-03-31 19:10:09 -0400

    finally figuring out options

--- shared/modules/gametest.nix
+++ shared/modules/gametest.nix
@@ -1,92 +1,58 @@
 { config, lib, pkgs, ...}: 
-let 
-  gcfg = config.gaming; 
-  ecfg = config.emulation;
-in {
+let
+  cfg = config.gaming;
+in{
 
-  options = { 
+  options.gaming = { 
+    # Minecraft
+    minecraft.enable = lib.mkEnableOption "Install Prism Launcher for Minecraft.";
 
-    emulation = {
-      switch.ryubing.enable = lib.mkEnableOption "Enable switch emulation with Ryubing.";
+    # Steam
+    steam.enable = lib.mkEnableOption "Install Steam.";
 
-      retroarch = {
-        enable = lib.mkEnableOption "Enable Retroarch software with some basic cores.";
-        extraCores = lib.mkOption {
-          type = lib.types.list;
-          default = [];
-          description = "Extra cores to install with Retroarch.";
-        };
+    # Mangohud
+    mangohud = {
+      enable = lib.mkEnableOption "Install Mangohud.";
+      config = lib.mkOption {
+        default = "
+          position=top-right,
+          frame_timing=0,
+          round_corners=10.0,
+          frametime=0,
+          hud_no_margin,
+          background_alpha=0,
+          table_columns=2
+        ";
+        type = lib.types.string;
+        description = "Mangohud config environment variable.";
       };
-
-      gba.mgba.enable = lib.mkEnableOption "Enable standalone mGBA emulator.";
     };
 
-    gaming = { 
-      
-      # Minecraft
-      minecraft.enable = lib.mkEnableOption "Install Prism Launcher for Minecraft.";
-
-      # Steam
-      steam.enable = lib.mkEnableOption "Install Steam.";
-
-      # Mangohud
-      mangohud = {
-        enable = lib.mkEnableOption "Install Mangohud.";
-        config = lib.mkOption {
-          default = "
-            position=top-right,
-            frame_timing=0,
-            round_corners=10.0,
-            frametime=0,
-            hud_no_margin,
-            background_alpha=0,
-            table_columns=2
-          ";
-          type = lib.types.string;
-          description = "Mangohud config environment variable.";
-        };
-      };
-
-      # ProtonUP
-      protonup.enable = lib.mkEnableOption "Install proton version manager.";
-    };
+    # ProtonUP
+    protonup.enable = lib.mkEnableOption "Install proton version manager.";
   };
 
   config = lib.mkMerge [
-    { environment.sessionVariables.HOWDY = "Howdy"; }
-    { environment.sessionVariables.PARTNER = "Partner"; }
-    { environment.sessionVariables.STEAMENABLED = "true"; }
-  ];
+    # Steam
+    (lib.mkIf cfg.steam.enable { programs.steam.enable = true; })
 
-  # config = lib.mkMerge [
-  #   lib.mkIf gcfg.steam.enable {
-  #     programs.steam.enable = true;
-  #   }
-  #
-  #   # Mangohud config
-  #   lib.mkIf gcfg.mangohud.enable {
-  #     environment.sessionVariables.MANGOHUD_CONFIG = gcfg.mangohud.config;
-  #     environment.systemPackages = [ pkgs.mangohud ];
-  #     # environment.sessionVariables = lib.mkIf gcfg.mangohud.enable {
-  #     #   MANGOHUD_CONFIG = gcfg.mangohud.config; 
-  #   }
-  #
-  #   # environment.systemPackages = lib.mkMerge [
-  #   #   lib.mkIf gcfg.mangohud.enable [pkgs.mangohud]
-  #   #   lib.mkIf gcfg.protonup.enable [pkgs.protonup-qt]
-  #   #   # lib.mkIf gcfg.minecraft.enable pkgs.prismlauncher
-  #   #   #
-  #   #   # lib.mkIf ecfg.switch.ryubing.enable pkgs.ryubing
-  #   #   # lib.mkIf ecfg.gba.mgba.enable pkgs.mgba
-  #   #   #
-  #   #   # lib.mkIf ecfg.retroarch.enable (
-  #   #   #   pkgs.retroarch.withCores (cores: with cores; [
-  #   #   #     melonds
-  #   #   #     mgba
-  #   #   #     citra
-  #   #   #   ])
-  #   #   # )
-  #   # ];
-  # ];
+    # Mangohud
+    (lib.mkIf cfg.mangohud.enable {
+      environment.systemPackages = [ pkgs.mangohud ];
+      environment.sessionVariables.MANGOHUD_CONFIG = cfg.mangohud.config;
+    })
+
+    # ProtonUP
+    (lib.mkIf cfg.protonup.enable {
+      environment.systemPackages = [ pkgs.protonup-qt ];
+    })
+
+    # Minecraft
+    (lib.mkIf cfg.minecraft.enable {
+      environment.systemPackages = [ pkgs.prismlauncher ];
+    })
+
+
+  ];
 
 }
