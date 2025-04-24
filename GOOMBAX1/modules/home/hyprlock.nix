00daca34943eb75d02233836e4f0838763eadf18
Gumbachi <jaredremsberg@gmail.com>
2025-04-23 20:53:12 -0400

    new file for nix

---
+++ GOOMBAX1/modules/home/hyprlock.nix
@@ -0,0 +1,102 @@
+{ config, ... }:
+let
+  textColor = if config.lib.stylix.enable then config.lib.stylix.colors.base05 else "cdd6f4";
+  accentColor = if config.lib.stylix.enable then config.lib.stylix.colors.base0D else "89b4fa";
+  font = "Inter";
+  monitor = "DP-1";
+in
+{
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      # GENERAL
+      general = {
+        disable_loading_bar = true;
+        hide_cursor = true;
+        ignore_empty_input = true;
+      };
+
+      # BACKGROUND
+      background = {
+        blur_passes = 2;
+      };
+
+      label = [ 
+        # TIME
+        {
+          monitor = "${monitor}";
+          text = ''cmd[update:30000] echo "$(date +"%R")"'';
+          color = "rgb(${textColor})";
+          font_size = 90;
+          font_family = "${font}";
+          position = "50, -30";
+          halign = "left";
+          valign = "top";
+        }
+
+        # DATE 
+        {
+          monitor = "${monitor}";
+          text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
+          color = "rgb(${textColor})";
+          font_size = 25;
+          font_family = "${font}";
+          position = "50, -180";
+          halign = "left";
+          valign = "top";
+        } 
+
+        # GREETING
+        {
+          monitor = "${monitor}";
+          text = "GOOMBAX1";
+          text_align = "center";
+          color = "rgb(${textColor})";
+          font_size = 60;
+          font_family = "${font}";
+          position = "0, 70"; # -80x for left align
+          halign = "center";
+          valign = "center";
+        }
+      ];
+
+      # USER AVATAR
+      image = {
+        monitor = "${monitor}";
+        path = builtins.toString ../../../images/avatars/5mort.jpg;
+        size = 400;
+        border_color = "rgb(${accentColor})";
+        border_size = 4;
+        position = "-500, 0";
+        halign = "center";
+        valign = "center";
+      };
+
+
+      # INPUT FIELD
+      input-field = {
+        monitor = "${monitor}";
+        size = "500, 100";
+        outline_thickness = 4;
+        dots_size = 0.25;
+        dots_spacing = 0.25;
+        dots_center = true;
+        # outer_color = $accent;
+        # inner_color = $surface0;
+        fade_on_empty = false;
+        placeholder_text = "$USER";
+        hide_input = false;
+        # check_color = $accent;
+        # fail_color = $red
+        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+        font_family = "${font}";
+        # font_color = $text
+        position = "0, -70";
+        halign = "center";
+        valign = "center";   
+      };
+
+    };
+  };
+}
