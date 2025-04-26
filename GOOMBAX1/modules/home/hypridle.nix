08e014e9c3f61ed30e70c99895ef8136cbf78a7f
Gumbachi <jaredremsberg@gmail.com>
2025-04-26 15:29:17 -0400

    add files for nix to work

---
+++ GOOMBAX1/modules/home/hypridle.nix
@@ -0,0 +1,34 @@
+{ ... }: {
+
+  programs.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        unlock_cmd = ''notify-send "unlock!"'';
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = true;
+        ignore_systemd_inhibit = false;
+      };
+
+      listener = [
+        {
+          timeout = 220;
+          on-timeout = ''notify-send -u normal "Screen locking in 20 seconds"'';
+        }
+        {
+          timeout = 240;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 900;
+          on-timeout = "systemctl poweroff";
+        }
+
+      ];
+
+    };
+  };
+}
+
