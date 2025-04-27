a2b06568bacd3ed63e504c30daa94a5e5b0c3669
Gumbachi <jaredremsberg@gmail.com>
2025-04-27 16:10:11 -0400

    commit for nix

---
+++ GOOMBAX1/modules/home/fish.nix
@@ -0,0 +1,31 @@
+{ pkgs, config, ... }: {
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      edit = "$EDITOR $CONFIG";
+      rebuild = "nh os switch";
+      rebuild-boot = "sudo nixos-rebuild boot";
+
+      sshs1 = "ssh jared@192.168.69.1";
+      sshs2 = "ssh jared@192.168.69.2";
+    };
+
+    shellAbbrs = {
+      nixgc = "sudo nix-collect-garbage -d";
+    };
+
+    shellInit = ''set fish_greeting'';
+
+    plugins = with pkgs.fishPlugins; [
+      transient-fish
+      sponge
+    ];
+  };
+
+  # Integrations
+  programs.starship.enableFishIntegration = config.programs.starship.enable;
+  programs.yazi.enableFishIntegration = config.programs.yazi.enable;
+  programs.zoxide.enableFishIntegration = config.programs.zoxide.enable;
+
+}
