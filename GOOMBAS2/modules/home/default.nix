918f102d5b0e3d68178115c4f9f7053e3281048f
Gumbachi <codeberg@gumbachi.com>
2025-06-13 22:20:30 -0400

    fix git identity

--- GOOMBAS2/modules/home/default.nix
+++ GOOMBAS2/modules/home/default.nix
@@ -10,6 +10,7 @@ in
 
     # Use modules from GOOMBAX1
     (GOOMBAX1 + /fish.nix)
+    (GOOMBAX1 + /git.nix)
     (GOOMBAX1 + /starship.nix)
     (GOOMBAX1 + /yazi.nix)
     (GOOMBAX1 + /kitty.nix)
