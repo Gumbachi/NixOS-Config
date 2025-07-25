4271071e67bb8e8df5755539eaf9a14eb1e69d1d
Gumbachi <codeberg@gumbachi.com>
2025-07-25 18:48:46 -0400

    change email back

--- GOOMBAX1/modules/home/git.nix
+++ GOOMBAX1/modules/home/git.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName = "Gumbachi";
-    userEmail = "codeberg@gumbachi.com";
+    userEmail = "github@gumbachi.com";
     extraConfig = {
       merge.ff = true;
     };
